e567e87b432c7c7c47528e6f78445ec9bd096793
Andrey Fedotov <fedotov_aa@outlook.com>
2022-03-15 11:18:09 +0300

    workflow_deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Новый релиз!')
+    return HttpResponse('У меня получилось! Новый релиз 2.0 УРА!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница! Новый релиз!')
+    return HttpResponse('А это вторая страница! Новый релиз 2.0 УРА!')
